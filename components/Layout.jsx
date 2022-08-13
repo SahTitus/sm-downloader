e207bf9e70e4c21f143f27ae910c2be765f06747
SahTitus <sahtitus58@gmail.com>
2022-08-13 17:32:22 +1200

    commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,10 @@ function Layout({ children }) {
     <div className="layout">
       <Head>
         <title>SM DOWNLOADER</title>
+        <link rel="icon" href="/android-chrome-maskable-512x512.png" />
+        <link rel="manifest" href="/manifest.json" />
+         <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+         <meta name="theme-color" content="#df517c" />
       </Head>
       <header>
         <Navbar />
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === 'development'
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+})
 
 module.exports = {
   images: {
